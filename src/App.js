d18294afb2940c020d0f790ed3f4906847931a5c
Oyizaadewuyi <oyizaadewuyi@gmail.com>
2024-07-09 21:49:29 +0100

    Timbu

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React from "react";
-import { BrowserRouter, Routes, Route,} from "react-router-dom";
-// import Header from "./components/Header/Header";
-// import Hero from "./components/Hero/Hero";
-// import Footer from "./components/Header/Footer/Footer";
-// import ProductPage from "./components/Product/ProductPage"
-import TimbuLanding from "./pages/TimbuLanding";
+import React from 'react'
 
-
-function App() {
+const App = () => {
   return (
     <div>
       
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<TimbuLanding />} />
-           {/* <Route path="Shoppingcart" element={<ShoppingCart/>} /> */}
-        </Routes>
-			</BrowserRouter>
-		</div>
-   
-       
-     
-  );
+    </div>
+  )
 }
-    
+
   
 
 
